52e91f808d320fe04691d0db137122017ada04a5
Alexander <eglasssus@gmail.com>
2019-12-25 17:18:42 +0300

    docs and tabs

--- src/02.Stack/02.Stack/Stack.h
+++ src/02.Stack/02.Stack/Stack.h
@@ -4,75 +4,75 @@ template <class ValType>
 class Stack
 {
 private:
-	int size;
-	ValType* elm;
-	int top;
+    int size;
+    ValType* elm;
+    int top;
 public:
-	Stack(int max);
-	Stack(const Stack&);
-	~Stack();
-	void Push(ValType);
-	void Pop();
-	bool IsEmpty() const;
-	bool IsFull() const;
-	ValType Top() const;
+    Stack(int max);
+    Stack(const Stack&);
+    ~Stack();
+    void Push(ValType);
+    void Pop();
+    bool IsEmpty() const;
+    bool IsFull() const;
+    ValType Top() const;
 };
 
 template <class ValType>
 Stack<ValType>::Stack(int max) : size(max), top(0)
 {
-	elm = new ValType[size];
+    elm = new ValType[size];
 }
 
 template <class ValType>
 Stack<ValType>::Stack(const Stack& c) : size(c.size), top(c.top)
 {
-	elm = new ValType[size];
-	for (int i = 0; i < top; i++)
-		elm[i] = c.elm[i];
+    elm = new ValType[size];
+    for (int i = 0; i < top; i++)
+        elm[i] = c.elm[i];
 }
 
 template <class ValType>
 Stack<ValType>::~Stack()
 {
-	delete[] elm;
+    delete[] elm;
 }
 
 template <class ValType>
 void Stack<ValType>::Push(ValType e)
 {
-	if (IsFull())
-		throw "Stack overflow";
-	elm[top++] = e;
+    if (IsFull())
+        throw "Stack overflow";
+    elm[top++] = e;
 }
 
 template <class ValType>
 void Stack<ValType>::Pop()
 {
-	if (IsEmpty())
-		throw "Empty Stack";
-	top--;
-	return;
+    if (IsEmpty())
+        throw "Empty Stack";
+    top--;
+    return;
 }
 
 template <class ValType>
 bool Stack<ValType>::IsEmpty() const
 {
-	if (top == 0) return true;
-	return false;
+    if (top == 0) return true;
+    return false;
 }
 
 template <class ValType>
 bool Stack<ValType>::IsFull() const
 {
-	if (top == size) return true;
-	return false;
+    if (top == size) return true;
+    return false;
 }
 
 template <class ValType>
 ValType Stack<ValType>::Top() const 
 {
-	if (IsEmpty())
-		throw "Empty Stack";
-	return elm[top - 1];
+    if (IsEmpty())
+        throw "Empty Stack";
+    return elm[top - 1];
 }
--- src/02.Stack/02.Stack/main.cpp
+++ src/02.Stack/02.Stack/main.cpp
@@ -5,48 +5,48 @@
 
 int main()
 {
-	int mode(2);
-	std::string s;
-	while (mode == 1 || mode == 2)
-	{
-		try
-		{
-			std::cout << "Input mode: 1 - manual input, 2 - auto input" << std::endl;
-			std::cout << "Input any other value to close the program" << std::endl;
-			std::cin >> mode;
-			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-			switch (mode)
-			{
-			case 1:
-			{
-				std::cout << "Input expression" << std::endl;
-				std::getline(std::cin, s);
-				std::string norm = PostfixForm::Normalize(s);
-				std::cout << "Normilized form: " << norm << std::endl;
-				std::string post = PostfixForm::Postfix(norm);
-				std::cout << "Postfix form: " << post << std::endl;
-				VarValues values(s);
-				values.InputValues();
-				std::cout << "Result " << PostfixForm::Compute(post, values) << std::endl;
-				break;
-			}
-			case 2:
-			{
-				std::cout << "A+B*C-D/E" << std::endl;
-				s = "A+B*C-D/E";
-				std::string post = PostfixForm::Postfix(s);
-				std::cout << "Postfix form: " << post << std::endl;
-				VarValues values(s);
-				values.InputValues();
-				std::cout << "Result " << PostfixForm::Compute(post, values) << std::endl;
-				break;
-			}
-			}
-		}
-		catch (const char* c)
-		{
-			std::cout << c << std::endl;
-		}
-	}
-	return 0;
+    int mode(2);
+    std::string s;
+    while (mode == 1 || mode == 2)
+    {
+        try
+        {
+            std::cout << "Input mode: 1 - manual input, 2 - auto input" << std::endl;
+            std::cout << "Input any other value to close the program" << std::endl;
+            std::cin >> mode;
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            switch (mode)
+            {
+            case 1:
+            {
+                std::cout << "Input expression" << std::endl;
+                std::getline(std::cin, s);
+                std::string norm = PostfixForm::Normalize(s);
+                std::cout << "Normilized form: " << norm << std::endl;
+                std::string post = PostfixForm::Postfix(norm);
+                std::cout << "Postfix form: " << post << std::endl;
+                VarValues values(s);
+                values.InputValues();
+                std::cout << "Result " << PostfixForm::Compute(post, values) << std::endl;
+                break;
+            }
+            case 2:
+            {
+                std::cout << "A+B*C-D/E" << std::endl;
+                s = "A+B*C-D/E";
+                std::string post = PostfixForm::Postfix(s);
+                std::cout << "Postfix form: " << post << std::endl;
+                VarValues values(s);
+                values.InputValues();
+                std::cout << "Result " << PostfixForm::Compute(post, values) << std::endl;
+                break;
+            }
+            }
+        }
+        catch (const char* c)
+        {
+            std::cout << c << std::endl;
+        }
+    }
+    return 0;
 }
